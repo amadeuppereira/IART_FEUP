ec245a122d21f867d0c2266d17550b1ae40c1ca8
Joao Alves <joao-carlos.alves@hotmail.com>
2019-03-15 02:43:01 +0000

    Moving single cell color working (first click with mouse and then move with arrow)

--- cohesion.py
+++ cohesion.py
@@ -9,16 +9,16 @@ import pygame
 class Cohesion:
 
 
-	#FIELD DIMENSIONS
-	ROWS = 4
-	COLUMNS = 4
+    #FIELD DIMENSIONS
+    ROWS = 4
+    COLUMNS = 4
 
-	DIRECTIONS = {
-		"UP": (0, 1),
-		"RIGHT": (1, 0),
-		"LEFT": (-1, 0),
-		"DOWN": (0, -1),
-	}
+    DIRECTIONS = {
+        "UP": (0, 1),
+        "RIGHT": (1, 0),
+        "LEFT": (-1, 0),
+        "DOWN": (0, -1),
+    }
 
 """
 
@@ -36,9 +36,29 @@ MARGIN = 40
 
 matrix = []
 for row in range(4):
-	matrix.append([])
-	for column in range(4):
-		matrix[row].append(0)
+    matrix.append([])
+    for column in range(4):
+        matrix[row].append(0)
+
+def handleRight(grid, x, y):
+    grid[x][y + 1] = grid[x][y]
+    grid[x][y] = 0
+    return grid
+
+def handleUp(grid, x, y):
+    grid[x-1][y] = grid[x][y]
+    grid[x][y] = 0
+    return grid
+
+def handleLeft(grid, x, y):
+    grid[x][y-1] = grid[x][y]
+    grid[x][y] = 0
+    return grid
+
+def handleDown(grid, x, y):
+    grid[x+1][y] = grid[x][y]
+    grid[x][y] = 0
+    return grid
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
@@ -47,18 +67,51 @@ pygame.display.set_caption('Cohesion')
 run = True
 clock = pygame.time.Clock()
 
+currentSelected = []
+matrix[0][0] = 1
+matrix[2][2] = 2
+
 #Main Loop
 while run:
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-	for row in range(4):
-		for column in range(4):
-			color = WHITE
-			pygame.draw.rect(screen, color, [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN, WIDTH, HEIGHT])
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            #click mouse and get position
+            pos = pygame.mouse.get_pos()
+            currentSelected.clear()
+            currentSelected.append(pos[1] // (WIDTH + MARGIN))
+            currentSelected.append(pos[0] // (WIDTH + MARGIN))
+
+    keys = pygame.key.get_pressed()
+
+    if len(currentSelected) == 2:
+
+        if keys[pygame.K_RIGHT]:
+            matrix = handleRight(matrix, currentSelected[0], currentSelected[1])
+            currentSelected = []
+        elif keys[pygame.K_UP]:
+            matrix = handleUp(matrix, currentSelected[0], currentSelected[1])
+            currentSelected = []
+        elif keys[pygame.K_LEFT]:
+            matrix = handleLeft(matrix, currentSelected[0], currentSelected[1])
+            currentSelected = []
+        elif keys[pygame.K_DOWN]:
+            matrix = handleDown(matrix, currentSelected[0], currentSelected[1])
+            currentSelected = []
+
+    for row in range(4):
+        for column in range(4):
+            color = WHITE
+            if matrix[row][column] == 1:
+                color = GREEN
+            elif matrix[row][column] == 2:
+                color = RED
+            pygame.draw.rect(screen, color, [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN, WIDTH, HEIGHT])
+    
     #limit to 60 frames per second
-	clock.tick(60)
+    clock.tick(60)
 
-	pygame.display.flip()
+    pygame.display.flip()
