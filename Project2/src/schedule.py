1eb80c15f0154de52441387119cc5124ef1c7a16
Amadeu Pereira <up201605646@fe.up.pt>
2019-05-15 11:09:37 +0100

    Incidence

--- Project2/src/schedule.py
+++ Project2/src/schedule.py
@@ -14,7 +14,7 @@ class Student:
 class Event:
     id = -1
     features = None
-    num_students = 0
+    students = None
     rooms = None
 
 ROOMS = []
@@ -51,17 +51,31 @@ for i, features in enumerate(events_features):
             e.features.append(j)
     EVENTS.append(e)
 
+# Get possible rooms for all events
 for event in EVENTS :
+    event.students = []
     for student in STUDENTS :
         if event.id in student.events :
-            event.num_students += 1
+            event.students.append(student.id)
+
     event.rooms = []
     for room in ROOMS :
-        if (room.size >= event.num_students) and (set(event.features).issubset(set(room.features))):
+        if (room.size >= len(event.students)) and (set(event.features).issubset(set(room.features))):
             event.rooms.append(room.id)
 
-for event in EVENTS :
-    print(event.id, event.features, event.num_students, event.rooms)
+INCIDENCE = [[False for x in range(len(EVENTS))] for y in range(len(EVENTS))]
+
+for e1 in EVENTS:
+    for e2 in EVENTS:
+        if not any(s in e2.students for s in e1.students):
+            INCIDENCE[e1.id][e2.id] = True
+
+for e1 in EVENTS:
+    if len(e1.rooms) == 1:
+        for e2 in EVENTS:
+            if len(e2.rooms) == 1 and e1.rooms[0] == e2.rooms[0]:
+                INCIDENCE[e1.id][e2.id] == False
+
 
 # print(filename)
 # print(timeslots)
